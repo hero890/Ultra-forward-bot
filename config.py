288020b7371f6533560b1f5734a17b3d9df95154
Tech HEro <159156837+hero890@users.noreply.github.com>
2024-03-14 16:11:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "22672907"))
     API_HASH = environ.get("API_HASH", "0ff15ae2153bd8e03b48cb293010bc6a")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7191328870:AAGdI04YGNdasxFik-YWVTEqXXoU1yK_V9s"") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7191328870:AAGdI04YGNdasxFik-YWVTEqXXoU1yK_V9s") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://thahero196:lP9Fb6aKL7T0y47U@cluster0.whs2bkj.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = environ.get("DATABASE_NAME", "")
